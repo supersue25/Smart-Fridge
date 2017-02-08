514ab0641ece7afec462c5f368102202ba778fe3
Superernest8 <des.ern8@gmail.com>
2017-02-09 00:21:49 +0530

    Add files via upload

---
+++ weight_double_sensors.ino
@@ -0,0 +1,36 @@
+/* FSR testing sketch. 
+ 
+Connect one end of FSR to 5V, the other end to Analog 0.
+Then connect one end of a 10K resistor from Analog 0 to ground
+Connect LED from pin 11 through a resistor to ground 
+ 
+For more information see www.ladyada.net/learn/sensors/fsr.html */
+ 
+int fsrAnalogPin = 0; // FSR is connected to analog 0
+int fsrAnalogPin1 = 1;
+int LEDpin = 11;      // connect Red LED to pin 11 (PWM pin)
+int fsrReading;      // the analog reading from the FSR resistor divider
+int fsrReading1;
+int LEDbrightness;
+ 
+void setup(void) {
+  Serial.begin(9600);   // We'll send debugging information via the Serial monitor
+  pinMode(LEDpin, OUTPUT);
+}
+ 
+void loop(void) {
+  fsrReading = analogRead(fsrAnalogPin);
+  fsrReading1 = analogRead(fsrAnalogPin1);
+  Serial.print("Analog reading = ");
+  Serial.println(fsrReading);
+   Serial.print("Analog only 1 reading = ");
+  Serial.println(fsrReading1);
+ 
+  // we'll need to change the range from the analog reading (0-1023) down to the range
+  // used by analogWrite (0-255) with map!
+  LEDbrightness = map(fsrReading, 0, 1023, 0, 255);
+  // LED gets brighter the harder you press
+  analogWrite(LEDpin, LEDbrightness);
+ 
+  delay(100);
+}
